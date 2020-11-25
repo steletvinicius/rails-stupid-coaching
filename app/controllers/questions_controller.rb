3268f96a4b366ee950bf8d15e5c1f4564afed62b
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-11-25 14:24:20 -0300

    Stupid coach coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,26 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
+  end
+
+  private
+
+  def coach_answer(question)
+    if question
+      if question == 'I am going to work'
+        'Great!'
+      elsif question.end_with?('?')
+        'Silly question, get dressed and go to work!'
+      else
+        "I don't care, get dressed and go to work!"
+      end
+    else
+      'Type a question.'
+    end
+  end
+
+end
